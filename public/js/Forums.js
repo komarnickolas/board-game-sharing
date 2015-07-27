5ecc4ee05c00d77e0da7de7dac8311f70e87d3d8
Wyatt Williams <williams.wyatt@gmail.com>
2015-07-27 13:47:44 -0700

    Just commiting so i can pull

--- public/js/Forums.js
+++ public/js/Forums.js
@@ -5,3 +5,23 @@ $(document).ready(function(){
     window.location.href = "#openModal";
   });
 });
+var Feedback = function(user, comment) {
+  this.user = user;
+  this.comment = comment
+}
+Feedback.prototype.render = function() {
+  var cell = document.createElement('tr');
+  cell.id = this.user;
+  var table = document.getElementById('comments');
+  cell.innerHTML = this.user;
+  table.appendChild(cell);
+}
+
+document.getElementById('btn').addEventListener('click'), function() {
+  var user = document.getElementById('user').value;
+  var comment = document.getElementById('comment').value;
+  var addcom = [user, comment]
+  var forum = new Feedback(addcom[0], addloc[1])
+  forum.render();
+  console.log("you wrote" + comment)
+}
