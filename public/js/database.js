f9eb993c8e0bc3325c50f8646f4c6709e0e60996
jypotheren734x <komarnickolas@gmail.com>
2015-07-27 12:41:05 -0700

    load db on all

---
+++ public/js/database.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  var db = 'https://api.mongolab.com/api/1/databases/users';
+  var collection = "/collections/usernames/55b6862ae4b077bc38f60527";
+  var apiKey = '?apiKey=CmxO8Pu1HeEpa6MSJyWa3ceKlKExom1_';
+  var userArray = [];
+  var getExistingUsers = $.ajax({
+      url:db + collection + apiKey,
+      type: "GET",
+      async: false,
+      contentType: "application/json",
+      dataType: 'json',
+      success: function(data)
+        {
+          console.log("Success");
+        }
+  });
+  console.log(getExistingUsers.responseText);
+  var usersToLoad = JSON.parse(getExistingUsers.responseText);
+  console.log(usersToLoad.username);
+  userArray = usersToLoad.username.userArray;
+  console.log(userArray);
+  var user = function(u,p){
+    this.username = u;
+    this.password = p;
+  };
+  $.ajax({
+    url: db + apiKey,
+    type:'GET',
+    success: console.log("Connected to: "+db)
+  });
+  $('#submit').click(function(){
+    userArray.push(new user($('#username').val(),$('#password').val()));
+    var data = JSON.stringify({username: {userArray}});
+    console.log(data);
+    console.log(db+"/collections/usernames"+apiKey);
+    $.ajax({
+      url:db+"/collections/usernames"+apiKey,
+      method: 'PUT',
+      data: data,
+      contentType: 'application/json',
+      success: console.log('success')
+    });
+  });
+}):
