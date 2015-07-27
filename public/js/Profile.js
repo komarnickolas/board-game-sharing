81ccf13eea04ded3082c99605f0a0371f476c1e4
James Bennett <J.bennett@gmx.com>
2015-07-27 14:37:42 -0700

    adds profile.html and links from all other htmls. adds placeholder fields on profile as well as a simple placeholder profile.js

---
+++ public/js/Profile.js
@@ -0,0 +1,13 @@
+(document).ready(function(){
+  $('Username').hide();
+  $('#Loginbtn').click(function(){
+    console.log('clicked');
+    window.location.href = "#openModal";
+  });
+  localStorage.setItem('SearchQuery',"");
+  $('#submitbtn').click(function(){
+    var searchQuery = $('#inputbox').val();
+    console.log(searchQuery);
+    localStorage.setItem('SearchQuery',searchQuery);
+  });
+});
