efc4d3329d67e63283e7a52b0c5cc05defd59a47
Wyatt Williams <williams.wyatt@gmail.com>
2015-07-27 14:22:41 -0700

    final

--- public/js/Forums.js
+++ public/js/Forums.js
@@ -5,23 +5,22 @@ $(document).ready(function(){
     window.location.href = "#openModal";
   });
 });
-var Feedback = function(user, comment) {
-  this.user = user;
-  this.comment = comment
+var Feedback = function(name, comments) {
+  this.name = name;
+  this.comments = comments;
+  this.arr = [];
 }
+
 Feedback.prototype.render = function() {
   var cell = document.createElement('tr');
   cell.id = this.user;
   var table = document.getElementById('comments');
   cell.innerHTML = this.user;
   table.appendChild(cell);
+
 }
 
 document.getElementById('btn').addEventListener('click'), function() {
   var user = document.getElementById('user').value;
   var comment = document.getElementById('comment').value;
-  var addcom = [user, comment]
-  var forum = new Feedback(addcom[0], addloc[1])
-  forum.render();
-  console.log("you wrote" + comment)
 }
