9180af9594591977f419d01f617ae92738620bf2
Wyatt Williams <williams.wyatt@gmail.com>
2015-07-28 09:15:10 -0700

    this sucks still

--- public/js/Forums.js
+++ public/js/Forums.js
@@ -3,26 +3,28 @@ $(document).ready(function(){
   $('#Loginbtn').click(function(){
     console.log('clicked');
     window.location.href = "#openModal";
-  });
-});
+  )};
+
 var Feedback = function(name, comments) {
   this.name = name;
   this.comments = comments;
   this.arr = [];
-}
+};
 
-Feedback.prototype.render = function() {
+Feedback.prototype.display = function() {
   var cell = document.createElement('tr');
-  cell.id = this.user;
   var table = document.getElementById('comments');
-  cell.innerHTML = this.user;
+  cell.innerHTML = this.name;
   table.appendChild(cell);
 };
 
-document.getElementById('btn').addEventListener('click'), function() {
-  var user = document.getElementById('user').value;
-  var comment = document.getElementById('comment').value;
-}
+$('#btn').click(function(){
+    console.log("clicked");
+    var user = document.getElementById('user').value;
+    var comment = document.getElementById('comment').value;
+    var newcomment = [name, comments];
+    var post = new Feedback(newcomment[0], newcomment[1]);
+    post.display();
+)};
 
-var post = new Feedback([0], [1]);
-post.render();
+)};
