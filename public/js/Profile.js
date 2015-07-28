4a874e49a200e82957644bf5431e6e211cfead78
jypotheren734x <komarnickolas@gmail.com>
2015-07-28 14:36:41 -0700

    js link fixed

--- public/js/Profile.js
+++ public/js/Profile.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
   $('#Loginbtn').hide();
   $('#addGame').hide();
+  $('#addGameBtn').click(function(){
+    $(this).fadeOut(1000);
+    $('#addGame').fadeIn(2000);
+  });
+  $('#addGameForum').click(function(){
+    console.log('adding game');
+  });
 });
