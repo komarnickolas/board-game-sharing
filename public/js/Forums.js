dae3161350ad999eb5c36c4a548d4612e7d66438
Wyatt Williams <williams.wyatt@gmail.com>
2015-07-28 15:40:23 -0700

    Ok, forum bulletin page is now operational

--- public/js/Forums.js
+++ public/js/Forums.js
@@ -4,12 +4,10 @@ $(document).ready(function(){
     console.log('clicked');
     window.location.href = "#openModal";
   });
-
 var Feedback = function(name, comments) {
   this.name = name;
   this.comments = comments;
 };
-
 Feedback.prototype.display = function() {
   var table = document.getElementById('table');
   var row = document.createElement('tr');
@@ -23,7 +21,6 @@ Feedback.prototype.display = function() {
   comm.innerHTML = this.comments;
   row.appendChild(comm);
   table.appendChild(row);
-
 };
 $('#btn').click(function(){
     var user = document.getElementById('user').value;
