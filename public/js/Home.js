288eb8abea2b34ec6bc85c379e901d7a43b2d9bc
James Bennett <J.bennett@gmx.com>
2015-07-29 13:17:44 -0700

    changes footer away from absolute, shoud fix to bottom.

--- public/js/Home.js
+++ public/js/Home.js
@@ -10,3 +10,23 @@ $(document).ready(function(){
     localStorage.setItem('SearchQuery',searchQuery);
   });
 });
+$('#searchbtn').click(function(e){
+    e.preventDefault();
+    var query = $('#title').val();
+    $('#title').val("");
+    for(var z = 0; z<tableSize; z++){
+      $('#newrow'+z).remove();
+    }
+    for(var x = 0; x<userArray.length; x++){
+      for(var y = 0; y<userArray[x].games.length; y++){
+        if(userArray[x].games[y].gameName.toLowerCase() === query.toLowerCase()){
+          addRow(x,y);
+          console.log('found');
+        }
+        else{
+          $('#newrow'+y).remove();
+          console.log('not found');
+        }
+      }
+    }
+  });
