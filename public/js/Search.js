19248b6ffa3d6d78231ca9cc37a1d0346836abfb
jypotheren734x <komarnickolas@gmail.com>
2015-07-29 10:20:40 -0700

    populate table

--- public/js/Search.js
+++ public/js/Search.js
@@ -7,4 +7,14 @@ $(document).ready(function(){
   if(retrievedSearchQuery != null){
     console.log(retrievedSearchQuery);
   }
+  for(var x = 0; x<userArray.length; x++){
+    for(var y = 0; y<userArray[x].games.length; y++){
+      $('#content').append('<tr id="newrow'+y+'"></tr>');
+      $('#newrow'+y).append('<td>'+userArray[x].games[y].gameName+'</td>');
+      $('#newrow'+y).append('<td>'+userArray[x].games[y].numberOfPlayers+'</td>');
+      $('#newrow'+y).append('<td>'+userArray[x].games[y].gameCondition+'</td>');
+      $('#newrow'+y).append('<td>'+userArray[x].games[y].gameStatus+'</td>');
+      $('#newrow'+y).append('<td>'+userArray[x].username+'</td>');
+    }
+  }
 });
