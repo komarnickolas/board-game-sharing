4515e6ed3e08b22d24a6cad05e9792f35c944cb3
jypotheren734x <komarnickolas@gmail.com>
2015-07-29 11:06:06 -0700

    search table works

--- public/js/Search.js
+++ public/js/Search.js
@@ -10,27 +10,35 @@ $(document).ready(function(){
   var tableSize = 0;
   for(var x = 0; x<userArray.length; x++){
     for(var y = 0; y<userArray[x].games.length; y++){
-      $('#content').append('<tr id="newrow'+y+'"></tr>');
-      $('#newrow'+y).append('<td id="'+y+'">'+userArray[x].games[y].gameName+'</td>');
-      $('#newrow'+y).append('<td>'+userArray[x].games[y].numberOfPlayers+'</td>');
-      $('#newrow'+y).append('<td>'+userArray[x].games[y].gameCondition+'</td>');
-      $('#newrow'+y).append('<td>'+userArray[x].games[y].gameStatus+'</td>');
-      $('#newrow'+y).append('<td>'+userArray[x].username+'</td>');
+      addRow(x,y);
       tableSize++;
     }
   }
   $('#searchbtn').click(function(){
     var query = $('#searchinput').val();
     $('#searchinput').val("");
-    for(var x = 0; x<tableSize; x++){
-      var currentRow = $('#'+x).text();
-      if(currentRow === query){
-        console.log('found');
-      }
-      else{
-        $('#newrow'+x).remove();
-        console.log('not found');
+    for(var z = 0; z<tableSize; z++){
+      $('#newrow'+z).remove();
+    }
+    for(var x = 0; x<userArray.length; x++){
+      for(var y = 0; y<userArray[x].games.length; y++){
+        if(userArray[x].games[y].gameName.toLowerCase() === query.toLowerCase()){
+          addRow(x,y);
+          console.log('found');
+        }
+        else{
+          $('#newrow'+y).remove();
+          console.log('not found');
+        }
       }
     }
   });
+  function addRow(x,y){
+    $('#content').append('<tr id="newrow'+y+'"></tr>');
+    $('#newrow'+y).append('<td id="'+y+'">'+userArray[x].games[y].gameName+'</td>');
+    $('#newrow'+y).append('<td>'+userArray[x].games[y].numberOfPlayers+'</td>');
+    $('#newrow'+y).append('<td>'+userArray[x].games[y].gameCondition+'</td>');
+    $('#newrow'+y).append('<td>'+userArray[x].games[y].gameStatus+'</td>');
+    $('#newrow'+y).append('<td>'+userArray[x].username+'</td>');
+  }
 });
