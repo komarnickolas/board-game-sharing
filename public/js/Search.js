a43ae29110289e4c0d6b7ad07b00d53d51d25667
jypotheren734x <komarnickolas@gmail.com>
2015-07-29 10:44:49 -0700

    forums head

--- public/js/Search.js
+++ public/js/Search.js
@@ -7,14 +7,26 @@ $(document).ready(function(){
   if(retrievedSearchQuery != null){
     console.log(retrievedSearchQuery);
   }
+  var tableSize = 0;
   for(var x = 0; x<userArray.length; x++){
     for(var y = 0; y<userArray[x].games.length; y++){
       $('#content').append('<tr id="newrow'+y+'"></tr>');
-      $('#newrow'+y).append('<td>'+userArray[x].games[y].gameName+'</td>');
+      $('#newrow'+y).append('<td id="'+userArray[x].games[y].gameName+'">'+userArray[x].games[y].gameName+'</td>');
       $('#newrow'+y).append('<td>'+userArray[x].games[y].numberOfPlayers+'</td>');
       $('#newrow'+y).append('<td>'+userArray[x].games[y].gameCondition+'</td>');
       $('#newrow'+y).append('<td>'+userArray[x].games[y].gameStatus+'</td>');
       $('#newrow'+y).append('<td>'+userArray[x].username+'</td>');
+      tableSize++;
     }
   }
+  $('#searchbtn').click(function(){
+    var query = $('#searchinput').val();
+    $('#searchinput').val("");
+    console.log(query);
+    for(var x = 0; x<tableSize; x++){
+      if($('#newrow'+x).val() === query){
+        console.log('found');
+      }
+    }
+  });
 });
