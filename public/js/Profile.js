439a9f445ba704470d478dd7fcfd3a86437b3f9d
jypotheren734x <komarnickolas@gmail.com>
2015-07-30 14:53:10 -0700

    pinging

--- public/js/Profile.js
+++ public/js/Profile.js
@@ -5,27 +5,34 @@ $(document).ready(function(){
     if(userArray[x].username === loggedInUser){
       for(var y = 0; y<userArray[x].games.length; y++){
         $('#tablebody').append('<tr id="newrow'+y+'"></tr>');
-        $('#newrow'+y).append('<td class="title">'+userArray[x].games[y].gameName+'</td>');
-        $('#newrow'+y).append('<td class="nop">'+userArray[x].games[y].numberOfPlayers+'</td>');
-        $('#newrow'+y).append('<td class="status">'+userArray[x].games[y].gameStatus+'</td>');
-        $('#newrow'+y).append('<td class="condition">'+userArray[x].games[y].gameCondition+'</td>');
-        $('#newrow'+y).append('<button id="'+y+'"> <image src="css/resources/minus.png"></image></button>');
+        $('#newrow'+y).append('<td id="title'+y+'">'+userArray[x].games[y].gameName+'</td>');
+        $('#newrow'+y).append('<td id="nop'+y+'">'+userArray[x].games[y].numberOfPlayers+'</td>');
+        $('#newrow'+y).append('<td id="status'+y+'">'+userArray[x].games[y].gameStatus+'</td>');
+        $('#newrow'+y).append('<td id="condition'+y+'">'+userArray[x].games[y].gameCondition+'</td>');
+        // $('#newrow'+y).append('<button id="'+y+'"> <image src="css/resources/minus.png"></image></button>');
       }
       if(userArray[x].pinged[0] === true){
         $('#ping').append('<l>'+userArray[x].pinged[1]+'</l>');
       }
     }
   }
-  $('button').click(function(){
-    var btnpressed = $(this).attr('id');
-    console.log(btnpressed);
-    for(var x = 0; x<userArray.length; x++){
-      if(userArray[x].username === loggedInUser){
-        
-      }
-    }
-    $(this).parent().remove();
-  });
+  // $('button').click(function(){
+  //   if($(this).attr('id') != $('#submitGamebtn').attr('id')){
+  //     var btnpressed = $(this).attr('id');
+  //     console.log(btnpressed);
+  //     for(var x = 0;x<userArray.length; x++){
+  //       if(userArray[x].username === loggedInUser){
+  //         for(var y = 0; y<userArray[x].games.length; y++){
+  //           if(userArray[x].games[y].gameName = $('#title'+y).text()){
+  //             userArray[x].games.splice(y,y);
+  //           }
+  //         }
+  //       }
+  //     }
+  //     $(this).parent().remove();
+  //     saveUsers();
+  //   }
+  // });
   var number = -1;
   $('#submitGamebtn').click(function(e){
     e.preventDefault();
@@ -45,9 +52,9 @@ $(document).ready(function(){
       }
     }
     console.log(number);
-    userArray[number].games.push(new game(newGameTitle, newGameStatus, newGameCondition, newMinGameNumberOfPlayers+"-"+newMaxGameNumberOfPlayers));
+    userArray[number].games.push(new game(newGameTitle, newGameStatus, newGameCondition, newMinGameNumberOfPlayers+"-"+newMaxGameNumberOfPlayers, number));
     console.log(userArray[number]);
-    $('#tablebody').append(render(newGameTitle, newGameStatus, newGameCondition, newMinGameNumberOfPlayers +"-"+ newMaxGameNumberOfPlayers));
+    $('#tablebody').append(render(newGameTitle, newGameStatus, newGameCondition, newMinGameNumberOfPlayers +"-"+ newMaxGameNumberOfPlayers, number));
     saveUsers();
     $('#gamesTitle').val("");
     $('#gamesNumberOfPlayers').val("");
